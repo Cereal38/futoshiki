d2ac643c69654b83768e434ae839df752a0c00c3
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-04-05 09:16:49 +0200

    Text constants and game page added

--- futoshiki/futoshiki.py
+++ futoshiki/futoshiki.py
@@ -1,9 +1,15 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
 from pcconfig import config
 
+# Futoshiki
 import pynecone as pc
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
+# Pages
+from futoshiki.pages.game import pageGame
+
+# Text
+from futoshiki.utils.text import TEXT_FUTOSHIKI_DESCRIPTION
+
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
@@ -16,11 +22,11 @@ class State(pc.State):
 def index() -> pc.Component:
     return pc.center(
         pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
+            pc.heading("Futoshiki", font_size="2em"),
+            pc.text(TEXT_FUTOSHIKI_DESCRIPTION, font_size='1em', width='80%'),
             pc.link(
-                "Check out our docs!",
-                href=docs_url,
+                "Start playing",
+                href='/game',
                 border="0.1em solid",
                 padding="0.5em",
                 border_radius="0.5em",
@@ -34,8 +40,8 @@ def index() -> pc.Component:
         padding_top="10%",
     )
 
-
 # Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index)
+app.add_page(index, route='/')
+app.add_page(pageGame, route='/game')
 app.compile()
---
+++ futoshiki/pages/game.py
@@ -0,0 +1,10 @@
+
+# Futoshiki
+import pynecone as pc
+
+
+def pageGame() -> pc.Component:
+  return pc.center(
+    pc.heading('Hello world', font_size='2em')
+  )
+
